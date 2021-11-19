f2f43496696d75bedd2c223cc15287f938bbe985
Ebenezer Nikabou <nikaboue10@gmail.com>
2021-11-19 10:46:55 +0000

    compose mail interface added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,82 @@
 <template>
   <div class="container" id="content">
-    <va-modal v-model="showModal" :message="message" title="Overview" />
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="composeMailModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Compose new mail</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                      <span class="fas fa-envelope"></span>
+                    </span>
+                  </div>
+                  <input type="email" placeholder="Recipient" class="form-control" />
+                </div>
+                <small id="emailHelp" class="form-text text-muted"
+                  >The recipient's email address.</small
+                >
+              </div>
+              <div class="form-group">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                      <span class="fas fa-users"></span>
+                    </span>
+                  </div>
+                  <input type="email" placeholder="Cc" class="form-control" />
+                </div>
+                <small id="emailHelp" class="form-text text-muted"
+                  >The addresses you want to follow the email.</small
+                >
+              </div>
+              <div class="form-group">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">
+                      <span class="fas fa-bullseye"></span>
+                    </span>
+                  </div>
+                  <input type="text" placeholder="Object" class="form-control" />
+                </div>
+                <small id="emailHelp" class="form-text text-muted"
+                  >Mail object or title.</small
+                >
+              </div>
+
+              <div class="form-group">
+                <textarea class="form-control" rows="3" placeholder="Mail content"></textarea>
+                <small id="emailHelp" class="form-text text-muted"
+                  >Your mail content goes here.</small
+                >
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">
+              <span class="fas fa-paper-plane"></span>
+              Send email
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div class="row no-gutters">
       <div class="col-lg-3">
@@ -74,7 +150,7 @@
       </div>
     </div>
   </div>
-  <button class="newMailButton" @click="showModal = !showModal">
+  <button type="button" data-toggle="modal" data-target="#composeMailModal" class="newMailButton">
     <span class="fas fa-plus"></span>
   </button>
 </template>
@@ -163,14 +239,14 @@ export default {
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  background-color: blueviolet;
+  background-color: #d9230f;
   color: #fff;
   position: absolute;
   right: 10%;
   bottom: 50px;
 }
 .mailActive {
-  background-color: blueviolet;
-  color: #fff;
+  background-color: #ffbcaf;
+  color: #333;
 }
 </style>
